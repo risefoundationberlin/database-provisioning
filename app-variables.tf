ac220cbeed1d35277b16795d2cc9a67bc2612c71
Piyush Sardana <piyush.sardana@risefoundation.de>
2022-03-11 10:01:52 +0100

    Created Postgress azure DB configuration

---
+++ app-variables.tf
@@ -0,0 +1,33 @@
+
+#############################
+## Application - Variables ##
+#############################
+
+variable "aks-vnet" {
+  type        = string
+  description = "existing aks vnet"
+  sensitive   = true
+}
+
+variable "aks-vnet-subnet" {
+  type        = string
+  description = "existing aks vnet"
+  sensitive   = true
+}
+
+variable "aks-vnet-resource-group" {
+  type        = string
+  description = "existing aks vnet resource group"
+  sensitive   = true
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region where the resource group will be created"
+  default     = "West Europe"
+}
+
+variable "postgres-resource-group" {
+  type        = string
+  description = "resource group where postgres components would be created"
+}
---
+++ main.tf
@@ -0,0 +1,58 @@
+########################################
+# Azure Database for PostgreSQL - Main #
+########################################
+
+provider "azurerm" {
+  features {}
+}
+
+# import existing subnet of aks cluster
+data "azurerm_subnet" "aks-subnet" {
+  name                 = var.aks-vnet-subnet
+  virtual_network_name = var.aks-vnet
+  resource_group_name  = var.aks-vnet-resource-group
+}
+
+# Create a PostgreSQL server
+resource "azurerm_postgresql_server" "postgresql-server" {
+  name                = "rf-postgresql-server"
+  location            = var.location
+  resource_group_name = var.postgres-resource-group
+
+  administrator_login          = var.postgresql-admin-login
+  administrator_login_password = var.postgresql-admin-password
+
+  sku_name = var.postgresql-sku-name
+  version  = var.postgresql-version
+
+  storage_mb        = var.postgresql-storage
+  auto_grow_enabled = true
+
+  backup_retention_days            = 7
+  geo_redundant_backup_enabled     = false
+  public_network_access_enabled    = true
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+  tags = {
+    Environment = "Production"
+  }
+}
+
+# Create a PostgreSQL database
+resource "azurerm_postgresql_database" "postgresql-db" {
+  name                = "event-management"
+  resource_group_name = var.postgres-resource-group
+  server_name         = azurerm_postgresql_server.postgresql-server.name
+  charset             = "utf8"
+  collation           = "English_United States.1252"
+
+}
+
+# Create a PostgreSQL virtual network rule
+resource "azurerm_postgresql_virtual_network_rule" "example" {
+  name                                 = "postgresql-vnet-rule"
+  resource_group_name                  = var.postgres-resource-group
+  server_name                          = azurerm_postgresql_server.postgresql-server.name
+  subnet_id                            = data.azurerm_subnet.aks-subnet.id
+  ignore_missing_vnet_service_endpoint = true
+}
